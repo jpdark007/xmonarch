3f51e11a2c919eadea1ea91eeabddda9bfea929f
Tran Nhut Trung <trannhuttrung@gmail.com>
2018-01-01 17:16:25 +0700

    Move declaration to .h

--- cryptonight/cryptonight.c
+++ cryptonight/cryptonight.c
@@ -8,13 +8,7 @@
 #include "jh/jh.h"
 #include "groestl/groestl.h"
 #include "oaes/oaes_lib.h"
-
-#define MEMORY         (1 << 21) /* 2 MiB */
-#define ITER           (1 << 20)
-#define AES_BLOCK_SIZE  16
-#define AES_KEY_SIZE    32 /*16*/
-#define INIT_SIZE_BLK   8
-#define INIT_SIZE_BYTE (INIT_SIZE_BLK * AES_BLOCK_SIZE)	// 128
+#include "cryptonight.h"
 
 void do_blake_hash(const void* input, size_t len, char* output) {
     blake(input, len, (unsigned char *)output);
@@ -41,33 +35,6 @@ void xor_blocks_dst(const uint8_t * a, const uint8_t * b, uint8_t * dst) {
 }
 
 #define likely(x) (x)
-
-#pragma pack(push, 1)
-union hash_state {
-  uint8_t b[200];
-  uint64_t w[25];
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-union cn_slow_hash_state {
-    union hash_state hs;
-    struct {
-        uint8_t k[64];
-        uint8_t init[INIT_SIZE_BYTE];
-    };
-};
-#pragma pack(pop)
-
-struct cryptonight_ctx {
-    uint8_t long_state[MEMORY] __attribute((aligned(16)));
-    union cn_slow_hash_state state;
-    uint8_t text[INIT_SIZE_BYTE] __attribute((aligned(16)));
-    uint8_t a[AES_BLOCK_SIZE] __attribute__((aligned(16)));
-    uint8_t b[AES_BLOCK_SIZE] __attribute__((aligned(16)));
-    uint8_t c[AES_BLOCK_SIZE] __attribute__((aligned(16)));
-    oaes_ctx* aes_ctx;
-};
 
 const uint32_t TestTable1[256]  __attribute((aligned(16))) ={
     0xA56363C6,0x847C7CF8,0x997777EE,0x8D7B7BF6,0x0DF2F2FF,0xBD6B6BD6,0xB16F6FDE,0x54C5C591,
