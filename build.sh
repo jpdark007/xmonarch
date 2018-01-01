a0f2cb589dd6fce4b20b78b07356b77b2d6e0479
Trung Tran <trannhuttrung@gmail.com>
2018-01-01 18:08:47 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # source $emsdk/emsdk_env.s
-echo ==========
+echo ================================================================================
 echo Make sure your Emscripten environment is configured properly 
-echo Please run 'source $EMSDK_HOME/emsdk_env.sh' before running this build script
+echo Please run \'source \$EMSDK_HOME/emsdk_env.sh\' before running this build script
 echo Otherwise, you will get error \'emcc: command not found\'
-echo ==========
+echo ================================================================================
 
 BUILD_DIR="_build"
 FILES="wasm/cn.c blake/blake.c cryptonight/cryptonight.c groestl/groestl.c jh/jh_ansi_opt64.c keccak/keccak.c oaes/oaes_lib.c skein/skein.c "
 FLAGS="-O3"
 FLAGS=""
 
-mkdir $BUILD_DIR
+mkdir -p $BUILD_DIR
 
 echo 
 echo "1. Build .wasm (and .js)"
